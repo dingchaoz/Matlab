6cfd2714fc0165596fcc1106abc527e2b5af8b78
Chris Remington <je113@cummins.com>
2014-04-30 17:33:58 -0400

    Changed CopyGUIToDLDiag to move RunCapabilityGUI.m one folder higher than all of the other files

--- CopyGUIToDLDiag.m
+++ CopyGUIToDLDiag.m
@@ -11,7 +11,7 @@ function CopyGUIToDLDiag
     copyList = {'@BoxPlotGenerator','@Capability','@CapHistGenerator','@DotPlotGenerator',...
                 '@HistogramGenerator','@StatCalculator','sqldriver','CapabilityGUI.fig',...
                 'CapabilityGUI.m','customDataCursor.m','findjobj.m','InstallSQLDriver.m',...
-                'rawData.fig','rawData.m','RunCapabilityGUI.m','xlswrite2.m'};
+                'rawData.fig','rawData.m','xlswrite2.m'};
     
    % For each file being worked on
     for i = 1:length(copyList)
@@ -21,7 +21,11 @@ function CopyGUIToDLDiag
         copyfile(copyList{i},fullfile(targetDir,copyList{i}),'f')
     end
     
+    % Copy the 'RunCapabilityGUI.m' file to one level higher directory
+    copyfile('RunCapabilityGUI.m',fullfile(targetDir,'..','RunCapabilityGUI.m'))
+    
     % Set all the files to be read-only to help prevent people from changing them (won't stop 
     % files from being deleted but this should help keep it from changing)
     dos(sprintf('attrib +r "%s\\*" /D /S',targetDir),'-echo');
+    dos(sprintf('attrib +r "%s\\..\\RunCapabilityGUI.m" /D /S',targetDir),'-echo');
 end
