a82ec972cc892a091958a1a1e3cadf59bbd04795
unknown <kz429@W3-R15471.ced.corp.cummins.com>
2014-06-20 16:55:35 -0400

    changes to batch files to let them use git

--- CapabilityETL.bat
+++ CapabilityETL.bat
@@ -1,12 +1,10 @@
-:: The bat file automatically calls CapabilityUploadScript.m from D:\Users\kz429\data crunching\GUI code\codeNew on W3-A22649 from
+:: The bat file automatically pulls the latest Capability scripts and
+:: calls CapabilityUploadScript.m from D:\Users\kz429\data crunching\GUI code\codeNew on W3-A22649 from
 :: the command line to run the capability upload script. This bat file is run via the task scheduler
-:: Author : Yiyuan Chen; Date: 2014/06/04
+:: Author : Yiyuan Chen; Date: 2014/06/20
 
-:: Change directory to D:\Users\kz429\data crunching\GUI code\codeNew
-d:
-cd Users\kz429\data crunching\GUI code\codeNew
+git.exe pull origin master
+git.exe pull --tags origin master
 
 :: Call Matlab script
-
 matlab -r CapabilityUploadScript
-
