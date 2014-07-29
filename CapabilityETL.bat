938a768eecbeb381e424cf096e21315a06696b87
unknown <kz429@W3-R15471.ced.corp.cummins.com>
2014-07-29 15:08:52 -0400

    modified the upload batch file

--- CapabilityETL.bat
+++ CapabilityETL.bat
@@ -1,17 +1,14 @@
 :: The bat file automatically pulls the latest Capability scripts and
 :: calls CapabilityUploadScript.m from D:\Users\kz429\data crunching\GUI code\codeNew on W3-A22649 from
 :: the command line to run the capability upload script. This bat file is run via the task scheduler
-:: Author : Yiyuan Chen; Date: 2014/06/20
-
-d:
-cd Users\kz429\data crunching\GUI code\codeNew 
-
-git.exe pull origin master
-git.exe pull --tags origin master
+:: Author : Yiyuan Chen; Date: 2014/07/29
 
 :: change the directory to D:\Users\kz429\data crunching\GUI code\codeNew
 d:
-cd Users\kz429\data crunching\GUI code\codeNew 
+cd Users\kz429\data crunching\GUI code\codeNew
+
+git.exe pull origin master
+git.exe pull --tags origin master
 
 :: Call Matlab script
 matlab -r CapabilityUploadScript
