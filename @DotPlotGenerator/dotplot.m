15b6e67cfb4e736cbee7e7fd1e41fb70f919002a
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2015-04-13 16:24:15 -0400

    Completed fault code matching for eventdriven diagnostics

--- @DotPlotGenerator/dotplot.m
+++ @DotPlotGenerator/dotplot.m
@@ -132,123 +132,80 @@ function separationLines = dotplot(obj,data,group,groupOrder,groupLabels,group2,
         
    % If fault code data is not empty
    if ~isempty(fcdata)
-    % Assign the fault code group data 
-    fcgroup = obj.FaultCode.TruckName;
-    
-    % Initalize fault code group index numbers
-    [~,fcgroupIndex] = ismember(fcgroup,groupOrder);
-    
-   end
-   %fcdata = obj.FaultCode.DataValue;
-    
-    % Empty array to hold the label names
-    %fclabels = {};
-    % Empty array to hold where separation lines need to be
-    %fcseparationLines = [];
-    
-    %t = cell2table({group,group2,groupIndex});
-    %t = {group,group2,groupIndex};
-    %t = horzcat(group,group2);
-    
-%     % Start at group number 1
-%     fcgroupNum = 1;
-%     % Start color selection index at 0
-%     %colorSel = 1;
-%     % Initalize group index numbers
-%     fcgroupIndex = zeros(size(fcdata));
-%     % initalize the color value of each data point
-%     %colorVal = zeros(size(data));
-%     
-%     %% Plot Each Data group
-%     
-%     % For each unique primary group present
-%     %for i = 1:length(obj.FaultCode.GroupOrder)
-%     for i = 1:length(groupOrder)
-%         
-%         % If there is a second group present
-%         if ~isempty(group2Order)
-%         
-% %         % If there is a second group present
-% %         if ~isempty(obj.FaultCode.Group2Order)
-% %             
-%             % For each second group present
-%             for j = 1:length(group2Order)        
-%       
-% %             % For each second group present
-% %             for j = 1:length(obj.FaultCode.Group2Order)
-%                 
-%                 % Find the index values in data of this combination
-%                 %idx = strcmp(obj.FaultCode.GroupOrder{i},obj.FaultCode.GroupData)&strcmp(obj.FaultCode.Group2Order{j},obj.FaultCode.Group2Data);
-%                 
-%                 idx = strcmp(groupOrder{i},obj.FaultCode.GroupData)&strcmp(group2Order{j},obj.FaultCode.Group2Data);
-%                 % If there was no data for this combination
-%                 if sum(idx) == 0
-%                     % Skip this vehicle
-%                     continue
-%                 end
-%                 % Set those index values to have the unique group number
-%                 fcgroupIndex(idx) = fcgroupNum;
-%                 % Set those index values to the current color selection
-%                 %colorVal(idx) = colorSel;
-%                 
-%                 % Define the label name
-%                 %fclabels{fcgroupNum} = sprintf('%s --- %s',obj.FaultCode.Labels{i},obj.FaultCode.Labels2{j});
-%                 
-%                 % Increment group number
-%                 fcgroupNum = fcgroupNum + 1;
-%                 
-%             end
-%             
-%             % Add this group to the separation lines
-%             %fcseparationLines = [fcseparationLines; fcgroupNum];
-%             % Make the label an empty set
-%             %fclabels{fcgroupNum} = [];
-%             % Increment group number
-%             %fcgroupNum = fcgroupNum + 1;
-%             % Increnent the current color
-%             %incColor;
-%             
-%         else % There is only one grouping
-%             
-%             % Get the index values in data of this combination
-%            % idx = strcmp(obj.FaultCode.GroupOrder{i},obj.FaultCode.GroupData);
-%             
-%             idx = strcmp(groupOrder{i},obj.FaultCode.GroupData);
-%             % If there was no data for this combination
-%             if sum(idx) == 0
-%                 % Skip this vehicle
-%                 continue
-%             end
-%             % Set those index values to have the unique group number
-%             fcgroupIndex(idx) = fcgroupNum;
-%             % Set those index values to a unique color
-%             %colorVal(idx) = colorSel;
-%             
-%             % Define the label name
-%             %fclabels{fcgroupNum} = sprintf('%s',obj.FaultCode.Labels{i});
-%             
-%             % Increment group number
-%             fcgroupNum = fcgroupNum + 1;
-%             % Increnent the current color
-%             %incColor;
-%             
-%             % If we're at the end add an empty label to the end just like the multi
-%             % grouping so that the bottom code works right
-%             %if i==length(obj.FaultCode.GroupOrder)
-%             if i==length(groupOrder)
-%                 
-%                 % Make the label an empty set
-%                 %fclabels{fcgroupNum} = [];
-%                 % Increment group number
-%                 fcgroupNum = fcgroupNum + 1;
-%             end
-%         end
-%     end
-%     
-%     
-% %             
-%     
+       
+        % Initalize fault code group index numbers   
+        fcgroupIndex = zeros(size(obj.FaultCode.GroupData));
 
+         %if there are 2 groups
+         if ~isempty(group2)
+
+
+                %generate the combined capability grouping indexes
+                t = table(group,group2,groupIndex);
+
+                %generate the unique capability grouping indexes
+                u_t = unique(t);
+
+                % Loop through the capability grouping index, to find matches of
+                % fault code grouping and return the fault code index
+
+                % Loop through the length of fault code matches
+                for i = 1 :length(fcgroupIndex)
+
+                  % if no fault code index has been matched yet( default value 0)
+                  while fcgroupIndex(i) == 0 
+
+                    %Loop through the unique capability grouping indexes
+                    for j = 1 : length(u_t{:,1})
+ 
+
+                        % if the first and second grouping matches, return the
+                        % index
+                        if strcmp(obj.FaultCode.GroupData(i),u_t{j,1}) && strcmp(obj.FaultCode.Group2Data(i),u_t{j,2});
+                            fcgroupIndex(i) = u_t{j,3};
+                            break;
+                        end   
+                    end
+                  end
+
+                end
+         else
+             
+                % if there is only one group
+                %generate the combined capability grouping indexes
+                t = table(group,groupIndex);
+
+                %generate the unique capability grouping indexes
+                u_t = unique(t);
+
+                % Loop through the capability grouping index, to find matches of
+                % fault code grouping and return the fault code index
+
+                % Loop through the length of fault code matches
+                for i = 1 :length(fcgroupIndex)
+
+                  % if no fault code index has been matched yet( default value 0)
+                  while fcgroupIndex(i) == 0 
+
+                    %Loop through the unique capability grouping indexes
+                    for j = 1 : length(u_t{:,1})
+
+
+                        % if the first and second grouping matches, return the
+                        % index
+                        if strcmp(obj.FaultCode.GroupData(i),u_t{j,1})
+                            fcgroupIndex(i) = u_t{j,2};
+                            break;
+                        end   
+                    end
+                  end
+
+                end
+             
+         end
+
+     end   
+         
     
     %% Plot the Data
     % Set the color map colors
