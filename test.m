ec286a643b8ba5dccb86d1e8cddec513b6177858
Dingchao Zhang <ks692@cummins.com>
2016-01-11 15:40:36 -0500

    finished mat Data uploading functions

--- test.m
+++ test.m
@@ -17,11 +17,12 @@
 
 function test(matfolder,file)
 
+program = 'DragonCC';
 
  % Define Conn
-% conn = database(program,'','','com.microsoft.sqlserver.jdbc.SQLServerDriver',...
-%            sprintf('%s%s;%s','jdbc:sqlserver://W4-S129433;instanceName=CapabilityDB;database=',program,...
-%             'integratedSecurity=true;loginTimeout=5;'));
+conn = database(program,'','','com.microsoft.sqlserver.jdbc.SQLServerDriver',...
+           sprintf('%s%s;%s','jdbc:sqlserver://W4-S129433;instanceName=CapabilityDB;database=',program,...
+            'integratedSecurity=true;loginTimeout=5;'));
  
 % form the full matfile path
 matfile = char(fullfile(matfolder,file));
@@ -29,52 +30,135 @@ matfile = char(fullfile(matfolder,file));
 % load the matfile into workspace
 load(matfile);
 
-dutyCycParams = {'abs_time','PC_Timestamp','ECM_Run_Time','ActiveFaults','Engine_Speed','Net_Engine_Torque','Accelerator_Pedal_Position'};
+dutyCycParams = {'TruckID','abs_time','PC_Timestamp','ECM_Run_Time',...
+    'Engine_Speed','Net_Engine_Torque','Accelerator_Pedal_Position',...
+    'Ambient_Air_Press','Coolant_Temperature','EGR_Position',...
+    'Boost_Pressure','Charge_Air_Cooler_Outlet_Tmptr','GPS_Altitude',...
+    'GPS_Latitude','GPS_Longitude','GPS_Speed'};
+
+
 
 % Get all the parameter names from the mat file
         allParams = who('-file', matfile);
 
         % Cell array to hold threshold values
-        Value = cell(1,length(dutyCycParams));
-
+%         Value = cell(1,length(dutyCycParams));
+        
         % Fill up value cell array
-        for i = 1:length(dutyCycParams)
-            
-            %% check if the duty cycle parameter exists in matfile
-            %% if yes, then move on to grab the data
-            %% else, insert '' and write to error log
-            %index = find(cellfun('length',regexp(allParams,dutyCycParams{1})) == 1)
-            index = find(ismember(allParams, dutyCycParams{i}));
-            
-            if ~isempty(index) 
-               
-                Value{i} = eval(allParams{index});
-           
-            else
-                % create an empty value cell array
-                Value{i} = cell(length(Value{1}),1);
-            end
-
-        end
+%         while item <= length(dutyCycParams) 
+%             
+%             %% check if the duty cycle parameter exists in matfile
+%             %% if yes, then move on to grab the data
+%             %% else, insert '' and write to error log
+%             %index = find(cellfun('length',regexp(allParams,dutyCycParams{1})) == 1)
+%            
+%             index = find(ismember(allParams, dutyCycParams{item}));
+%            
+%             
+%             if ~isempty(index) 
+%                
+%                 Value{item} = eval(allParams{index});
+%            
+%             else
+%                 % create an empty value cell array
+%                 Value{item} = cell(length(Value{2}),1);
+%             end
+%             
+%             item = item +1;
+% 
+%         end
+        
+        %print('finished');
         
          % Create calTable struct to hold table to be inserted
-        %calTable = struct('Family',{},'CalVersion',{},'CalRev',{},'Threshold',{},'Value',{});
-        % needs to add the truckname
-        %matTable = struct('TruckID',{},'abs_time',{},'PC_Timestamp',{},'ECM_Run_Time',{},'ActiveFaults',{},'Engine_Speed',{},'Net_Engine_Torque',{},'Accelerator_Pedal_Position',{});
+        matTable = struct('TruckID',{},'abs_time',{},'PC_Timestamp',{},...
+            'ECM_Run_Time',{},'Engine_Speed',{},'Net_Engine_Torque',{},...
+            'Accelerator_Pedal_Position',{},'Ambient_Air_Press',{},...
+            'Coolant_Temperature',{},'EGR_Position',{},'Boost_Pressure',{},...
+            'Charge_Air_Cooler_Outlet_Tmptr',{},'GPS_Altitude',{},...
+            'GPS_Latitude',{},'GPS_Longitude',{},'GPS_Speed',{});
         
-%         % Fill up the structure of Caltable
-%         calTable(1).Threshold = Threshold;
-%         calTable(1).Value = Value;
-%         calTable(1).Family = Family;
-%         calTable(1).CalVersion = CalVersion;
-%         calTable(1).CalRev = CalRev;
+ 
+         %% Fill up the cell array with strings of values
+        TruckID = cell(1,length(abs_time));
+        TruckID(1,:) = cellstr('1');
+        %abs_time = cell(1,length(Value{2}));
+        %abs_time(1,:) = cellstr(num2str(Value{2}));
+        %% PC time stamp truckID ok because cell
+     
+%      % Fill up the structure of matTable
+        matTable(1).TruckID = TruckID';
+       
+        fields = fieldnames(matTable);
+
+        for i = 2:numel(fields)
+            
+            index = find(ismember(allParams, dutyCycParams{i}));
+            
+            % if the parameter can be found and the parameter has values
+            % collected
+            if ~isempty(index) 
+              
+                if length(eval(allParams{index})) > 0 
+                    
+                    matTable.(fields{i}) = eval(allParams{index});
+                else
+                    % create a cell array of the same length as the abs_time
+                    var = cell(1,length(abs_time));
+
+                    % if the parameter is a cell, create empty string cell
+                    if iscell(eval(allParams{index}))
+
+                        var(1,:) = cellstr('');
+
+                    % if the parameter is a double, then put some unlikely value in the cell
+                    % this is not an optimal approach, for GPS coordiante, we
+                    % choose -1 to be put in as GPS coordiante can't be -1
+                    elseif isa(eval(allParams{index}),'double')
+                        var(1,:) = num2cell(-1);
+                    end
+
+                    % transpose
+                    var = var';
+                    % put the cell array into struct
+                    matTable.(fields{i}) = var;
+                    
+                end
+            % if the parameter can't be found, to write an error export
+            % function and specify the error message specifically which
+            % parmeter is missing
+            else 
+                
+                print('parameter is missing from this matfile')
+                
+            end
+        
+        end
+        
+         % Upload the data and engine family to the database
+        fastinsert(conn,'[dbo].[tblMatData]',fieldnames(matTable),matTable);
+        
+        % Close the database connection
+        close(conn)
+        
+        % Else print cal already uploaded    
+        fprintf('Mat file %s is uploaded to database matData table.\n',file,program);
+      
+%         matTable(1).abs_time = abs_time;
+%         matTable(1).PC_Timestamp = PC_Timestamp;
+%         matTable(1).ECM_Run_Time = ECM_Run_Time;
+%         matTable(1).Engine_Speed = Engine_Speed;
+%         matTable(1).Net_Engine_Torque = Net_Engine_Torque;
+%         matTable(1).Accelerator_Pedal_Position = Accelerator_Pedal_Position;
+%         matTable(1).Ambient_Air_Press = Ambient_Air_Press;
+%         matTable(1).Coolant_Temperature = Coolant_Temperature;
+%         matTable(1).Net_Engine_Torque = Net_Engine_Torque;
+%         matTable(1).EGR_Position = EGR_Position;
+%         matTable(1).Boost_Pressure = Boost_Pressure;
+%         matTable(1).Charge_Air_Cooler_Outlet_Tmptr = Charge_Air_Cooler_Outlet_Tmptr;
+%         matTable(1).GPS_Longitude = GPS_Longitude;
+%         matTable(1).GPS_Latitude = GPS_Latitude;
+%         matTable(1).GPS_Speed = GPS_Speed;
 % 
-%         % Upload the data and engine family to the database
-%         fastinsert(conn,'[dbo].[tblCals1]',fields(calTable),calTable);
 %         
-%         % Close the database connection
-%         close(conn)
-%     
-%         % Else print cal already uploaded    
-%         fprintf('Cal Version %s and Revision %s is uploaded to database cal table in Family %s of program %s.\n',calVersion,calRev, family,program);
 end
