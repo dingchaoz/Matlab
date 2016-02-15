f7a2cb014ccefb5d1d80d60486fa08ef09a5d07b
Dingchao Zhang <ks692@cummins.com>
2016-02-15 11:02:16 -0500

    modified scripts to make it work for PU matdata uploading as well as Front

--- @MatfileUploader/matdataUploader.m
+++ @MatfileUploader/matdataUploader.m
@@ -23,50 +23,41 @@ function matdataUploader(obj)
     filesEt = 0;
     timeEt = 0;
 
-    %% Fetch the maximum conditionID for EFI to process and upload new data only
-%     if ismember(obj.program,{'DragonCC','DragonMR','Seahawk'})
-%         % Pick the biggest ConditionID in MinMax Data table before uploading new EFI data 
-%         % so that only the EFI data uploaded this time will be processed, as below 
-%         EFImaxConditionID = cell2mat(struct2cell(fetch(obj.conn, 'SELECT Max(ConditionID) FROM dbo.tblMinMaxData')));
-%     else
-%     end
     
     %% Automated File Finder and Mover
     totalStart = tic;
     
-%     % Calculate the root directory of the data
-%     if strcmp(obj.program,'HDPacific')  % Override HDPacific to be the Pacific folder 
-%         startDir = '\\CIDCSDFS01\EBU_Data01$\NACTGx\fngroup_ctc\ETD_Data\MinMaxData\Pacific';
-% 
-%     elseif strcmp(obj.program,'Pele')  % Override Pele to be the PeleII folder
-%         startDir = '\\CIDCSDFS01\EBU_Data01$\NACTGx\fngroup_ctc\ETD_Data\MinMaxData\PeleII';
-%     else  % Use the program name for all the rest
-% 
-%         startDir = ['\\CIDCSDFS01\EBU_Data01$\NACTGx\fngroup_ctc\ETD_Data\MinMaxData\' obj.program];
-%     end
-%     
     
     % Recurse one level through the program directory folders on MR because the data is everywhere
     base = '\\CIDCSDFS01\EBU_Data01$\NACTGx\mrdata';
+    
     % Program names as in the database
-    prog =     {'DragonCC',   'DragonMR', 'Seahawk', 'Yukon' ,'Nighthawk','Sierra','Vulture','Thunderbolt'};
+    prog = obj.program;
+    
     % Program names as in the mrdata folder
-    progFold = {'DragonFront','Dragon',   'Seahawk', 'Yukon' ,'Nighthawk','Sierra','Vulture','Thunderbolt'};
+    if strcmp(prog,'DragonCC')
+        progFold = 'DragonFront';
+    elseif strcmp(prog,'DragonCC')
+        progFold = 'Dragon';
+    else
+        progFold = prog;
+    end
+
     % Empty variable to hold all the driectories
     programs = {};
-    for i = 1:length(prog)
-        % Add the base directory
-        programs = [programs;prog(i),{fullfile(base,progFold{i})}];
-        % Find one level deeper of folder names
-        nextLevel = dir(fullfile(base,progFold{i}));
-        % Keep only the directories
-        nextLevel = nextLevel(cell2mat({nextLevel(:).isdir}));
-        % Loop through each folder (excluding . and ..)
-        for j = 3:length(nextLevel)
-            % Add each to the program folders to look through
-            programs = [programs;prog(i),{fullfile(base,progFold{i},nextLevel(j).name)}];
-        end
+    
+     % Add the base directory
+    programs = [programs;prog,{fullfile(base,progFold)}];
+    % Find one level deeper of folder names
+    nextLevel = dir(fullfile(base,progFold));
+    % Keep only the directories
+    nextLevel = nextLevel(cell2mat({nextLevel(:).isdir}));
+    % Loop through each folder (excluding . and ..)
+    for j = 3:length(nextLevel)
+        % Add each to the program folders to look through
+        programs = [programs;prog,{fullfile(base,progFold,nextLevel(j).name)}];
     end
+    
     
         % For each program
     for item = 1:size(programs,1)
@@ -94,11 +85,6 @@ function matdataUploader(obj)
 
             currentFolder = fullfile(parentFolder,truckList,'MatData_Files');
             
-%             % Remove the folder which is not a matfiles folder
-%             while ~exist(currentFolder{i})
-%                currentFolder(i) = [];
-%                i++;
-%             end
 
             for i = 1:length(currentFolder)
                 
@@ -172,7 +158,7 @@ function matdataUploader(obj)
                     
                     % If the folder is the same with the latest processed
                     % folder
-                    elseif yr == maxYr && mth == maxMth
+                    elseif yr == maxYr && mth >= maxMth -1
                         
                         new = false;
                         ProcessMat(matFolder{j},new);
@@ -181,23 +167,9 @@ function matdataUploader(obj)
                     else
                         continue
                     end
-                    %% First check if this folder directory is already recorded in the tblProcessedMatFiles FilePath or not
-                    %% if it exists, select all FileNames in that folder, see if the file name is recorded or not, if not recorded,
-                    %% then go ahead process
+     
                     
-%                     maxFID = fetch(obj.conn, sprintf('SELECT max([FileID]) FROM [dbo].[tblProcessedMatFiles] where TruckID = %i',truckID));
-%                     lastFInfo = fetch(obj.conn, sprintf('SELECT * FROM [dbo].[tblProcessedMatFiles] where FileID = %i', maxFID.x));
-                    
-                    % Fetch the latest month and year of the truck process
-                    % mat file
-                   % EFImaxConditionID = cell2mat(struct2cell(fetch(obj.conn, 'SELECT Max(ConditionID) FROM dbo.tblMinMaxData')));
-                    
-%                     if ~obj.getTruckProcessData(truckID)
-%                         % Write a warning log entry
-%                         obj.warning.write(sprintf('==========> Data processing on truck %s is being skipped.',truckList{i}));
-%                         % Continue on to the next vehicle
-%                         continue
-%                     end
+
                     
                 end    
 
@@ -224,8 +196,9 @@ function matdataUploader(obj)
                     
                     filePth = ['''',matFolder,''''];
                     processedFiles = fetch(obj.conn, sprintf('SELECT distinct FileName FROM [dbo].[tblProcessedMatFiles] where TruckID = %i and FilePath = %s',truckID,filePth));
+                    erroredFiles = fetch(obj.conn, sprintf('SELECT distinct FileName FROM [dbo].[tblErroredMatFiles] where TruckID = %i',truckID));
                     % Check if new file exists, if already exists, skip it
-                    if find(ismember(processedFiles.FileName,file))
+                    if find(ismember(processedFiles.FileName,file)) || find(ismember(erroredFiles.FileName,file))
                             
                         fprintf('No new mat file in     %s\r',char(matFolder));
                         
@@ -234,23 +207,28 @@ function matdataUploader(obj)
                         fprintf('There is new mat file in     %s\r',char(matFolder));
                         
                         % Process and read the mat file
-                        readMatFile(obj,matFolder,file,truckID,programs{item,1});
-                              
-                        % Insert file and update file ID 
-                        addProcessedMat(obj,file,matFolder,truckID,mth,yr);
-                        % There were no Min/Max files present, display a message
-                        fprintf('Mat file %s in %s is processed and updated in the tblProcessedMatFiles\r',char(file),char(matFolder));
+                        read = readMatFile(obj,matFolder,file,truckID,programs{item,1});
+                        
+                        if read == 1
+                            % Insert file and update file ID 
+                            addProcessedMat(obj,file,matFolder,truckID,mth,yr);
+                            % There were no Min/Max files present, display a message
+                            fprintf('Mat file %s in %s is processed and updated in the tblProcessedMatFiles\r',char(file),char(matFolder));
+                        end
                     end
                     
                 else
                     
                     % Process and read the mat file
-                    readMatFile(obj,matFolder,file,truckID,programs{item,1});
-                              
-                    % Insert file and update file ID 
-                    addProcessedMat(obj,file,matFolder,truckID,mth,yr);
-                    % There were no Min/Max files present, display a message
-                    fprintf('Mat file %s in %s is processed and updated in the tblProcessedMatFiles\r',char(file),char(matFolder(j)));
+                    read = readMatFile(obj,matFolder,file,truckID,programs{item,1});
+                    
+                    % If the matfile is read and matdata is uploaded
+                    if read == 1
+                        % Insert file and update file ID 
+                        addProcessedMat(obj,file,matFolder,truckID,mth,yr);
+                        % There were no Min/Max files present, display a message
+                        fprintf('Mat file %s in %s is processed and updated in the tblProcessedMatFiles\r',char(file),char(matFolder(j)));
+                    end
                     
                 end
    
@@ -260,46 +238,7 @@ function matdataUploader(obj)
             fprintf('No mat files in     %s\r',char(matFolder));
        end                
     end
-    
-%     % Blank array for folder to work on
-%     truckList = {};
-%     % Look for all truck folders that exist
-%     truckDirData = dir(startDir);
-%     % For each listing in the directory
-%     for i = 3:length(truckDirData)
-%         % If this was a folder (not a file inappropriatly placed in the wrong directory)
-%         if truckDirData(i).isdir
-%             % Append this onto the truckList
-%             truckList = [truckList;{truckDirData(i).name}];
-%         end
-%     end
-    
-%     % For each truck that has data
-%     for i = 1:length(truckList)
-%         % Try to get the truck ID of this truck
-%         try
-%             truckID = obj.getTruckID(truckList{i});
-%         catch ex
-%             if strcmp('Capability:getTruckID:NoMatch',ex.identifier)
-%                 % Add this truck name to the tblTrucks table
-%                 truckID = obj.addTruck(truckList{i});
-%                 
-%                 % Log that this truck name was added to the table
-%                 obj.error.write('-----------------------------------------------------------');
-%                 obj.error.write(['Added entry to trucks table for vehicle ' truckList{i} '.']);
-%             else
-%                 % Rethrow the original, unknown exception
-%                 rethrow(ex)
-%             end
-%         end
-%         
-%         % Check if data should be processed for this truck
-%         if ~obj.getTruckProcessData(truckID)
-%             % Write a warning log entry
-%             obj.warning.write(sprintf('==========> Data processing on truck %s is being skipped.',truckList{i}));
-%             % Continue on to the next vehicle
-%             continue
-%         end
+
 %         
 %         % Assign the workingDir
 %         workingDir = fullfile(startDir, truckList{i});
@@ -335,24 +274,4 @@ function matdataUploader(obj)
 %     obj.timer.write(sprintf('Total Files Processed:    %4.0f files in %5.1f seconds = %.2f seconds per file.',filesPt+filesEt,timePt+timeEt,(timePt+timeEt)/(filesPt+timeEt)));
 %     
 
-%     %% Call sqlcmd
-%     % Split current directory into parts
-%     %fileParts = toklin(pwd,'\');
-%     % Calculate full path to the .sql file
-%     %sqlFilePath = fullfile(fileParts{1:end-1},'suppdata',obj.program,[obj.program '_FixData.sql']);
-%     % New location on the network
-%     sqlFilePath = fullfile('\\CIDCSDFS01\EBU_Data01$\NACTGx\common\DL_Diag\Data Analysis\Storage\suppdata',obj.program,[obj.program '_FixData.sql']);
-%     % Check that the file exists
-%     if exist(sqlFilePath,'file')
-%         % Display that the sql script is being called
-%         disp(['Running ' obj.program '_FixData.sql....'])
-%         % Execute the [program]_FixData.sql commands after new data has been uploaded to the database
-%         dos(sprintf('sqlcmd -S tcp:%s\\%s -E -i \"%s\"',obj.server,obj.instanceName,sqlFilePath))
-%     else
-%         % Warn that this couldn't be run
-%         disp('Failed to run the data cleaning .sql script')
-%         % Log the error
-%         obj.error.write('-----------------------------------------------------------');
-%         obj.error.write('Failed to find the FixData.sql file')
-%     end
 end
